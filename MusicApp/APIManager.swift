25bab1be0b3f31f2b4118bbe91071a7f738562e7
Jyhcheng Liu <tomjliu@gmail.com>
2016-05-03 22:08:20 -0400

    Added Json task

---
+++ MusicApp/APIManager.swift
@@ -0,0 +1,57 @@
+//
+//  APIManager.swift
+//  MusicApp
+//
+//  Created by Grants Office on 5/3/16.
+//  Copyright © 2016 Grants Office. All rights reserved.
+//
+
+import Foundation
+
+class APIManager {
+    
+    func loadData(urlString:String, completion: (result:String) ->  Void) {
+        
+        let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        
+        let session = NSURLSession(configuration: config)
+        
+       // let session = NSURLSession.sharedSession()
+        let url = NSURL(string: urlString)
+        
+        let task = session.dataTaskWithURL(url!) {
+            (data, response, error) -> Void in
+            
+           // print(response)
+            
+                if error != nil {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completion(result: (error!.localizedDescription))
+                    }
+                } else {
+                   // completion(result: "NSURLSession successful")
+                    //print(data)
+                    do {
+                        
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject] {
+                            
+                            
+                            print(json)
+                            
+                            let priority = DISPATCH_QUEUE_PRIORITY_BACKGROUND
+                            dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                                dispatch_async(dispatch_get_main_queue()) {
+                                    completion(result: "JSONSerialization sucessful")
+                                }
+                            }
+                        }
+                    } catch {
+                dispatch_async(dispatch_get_main_queue())  {
+                    completion(result: "error in NSJSONSerialization")
+                }
+                    }
+            }
+        }
+        task.resume()
+    }
+}
