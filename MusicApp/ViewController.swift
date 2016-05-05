29e6eab86d0c20533a7d19fca6c3a4210d12eccc
Jyhcheng Liu <tomjliu@gmail.com>
2016-05-04 22:16:45 -0400

    Added reachability files and code to check for internet connection

--- MusicApp/ViewController.swift
+++ MusicApp/ViewController.swift
@@ -12,19 +12,46 @@ class ViewController: UIViewController {
 
     var videos = [Videos]()
     
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(reachabilityStatusChanged), name: "ReachStatusChanged", object: nil)
+        
+        reachabilityStatusChanged()
+        
         let api = APIManager()
         api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
     }
 
     func didLoadData(videos: [Videos]){
         
+        print(reachabilityStatus)
+        
         for (index, item) in videos.enumerate() {
             print("\(index) name = \(item.vName)")
         }
         
+    }
+    
+    func reachabilityStatusChanged() {
+        
+        switch reachabilityStatus {
+        case NOACCESS: view.backgroundColor = UIColor.redColor()
+            displayLabel.text = "NO internet"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+            displayLabel.text = "Reachable with WIFI"
+        case WWAN : view.backgroundColor = UIColor.yellowColor()
+            displayLabel.text = "Reachable with Cellular"
+        default:
+            return
+        }
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
     }
     
     override func didReceiveMemoryWarning() {
